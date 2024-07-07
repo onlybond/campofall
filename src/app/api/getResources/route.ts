ae2250f70a78566507ccdd3b9f860f722c520f34
bibin thomas <bibinthomas951@gmail.com>
2024-07-07 23:57:39 +0530

    fix: fixed dynamicserver error

--- src/app/api/getResources/route.ts
+++ src/app/api/getResources/route.ts
@@ -2,14 +2,14 @@ import dbConnect from "@/lib/database";
 import { NextResponse, NextRequest } from "next/server";
 import resourceModel from "@/app/models/resource";
 import APIFeatures from "@/lib/apiFeatures";
-
+import { isDynamicServerError } from "next/dist/client/components/hooks-server-context";
 export async function GET(request: NextRequest) {
   await dbConnect();
 
+  const queryString = Object.fromEntries(
+    request.nextUrl.searchParams.entries()
+  );
   try {
-    const queryString = Object.fromEntries(
-      request.nextUrl.searchParams.entries()
-    );
     const features = new APIFeatures(resourceModel.find(), queryString)
       .filter()
       .sort()
@@ -20,6 +20,9 @@ export async function GET(request: NextRequest) {
 
     return NextResponse.json({ success: true, data: resources });
   } catch (error) {
+    if (isDynamicServerError(error)) {
+      throw error;
+    }
     return NextResponse.json({
       success: false,
       message: "Error fetching resources",
